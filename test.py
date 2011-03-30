7c1abf9e195c9777f88240139c8d6a83f6933a0b
Joseph Abrahamson <abrahamson.j@gmail.com>
2011-03-30 19:51:53 -0400

    Updated test file

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
 import numpy as np
 import main
 import jumps
+import ab
 import Image
 
-foo = lambda: jumps.jump_matrices(main.d.baseforms[0], main.fenomes)
+def do():
+    ab.train(main.d.baseforms[0], main.fenomes, main.d.observations[0])
 
-a, b, c, d, e = foo()
-Image.fromarray(np.uint8(255*e/np.max(e))).save('out.png')
+def test_jumps():
+    foo = lambda: jumps.jump_matrices(main.d.baseforms[0], main.fenomes)
+    a, b, c, d, e = foo()
+    Image.fromarray(np.uint8(255*e/np.max(e))).save('out.png')
+
+
+if __name__ == '__main__':
+    do()
